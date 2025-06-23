ac351a1852314d15bf01e01c6851fedc28bf5fb4
0margarita0 <165337182+0margarita0@users.noreply.github.com>
2025-06-23 23:19:01 +0200

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab04.Models;
+
+namespace Lab04.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> eventModel = new List<EventModel>()
+        {
+            new EventModel()
+            {
+                ID = 1,
+                name = "",
+                location = ""
+            },
+            new EventModel()
+            {
+                ID = 2,
+                name = "",
+                location = ""
+            },
+            new EventModel()
+            {
+                ID = 3,
+                name = "",
+                location = ""
+            }
+        };
+        // GET: Event
+        public ActionResult EventView()
+        {
+            Events events = new Events();
+            events.eventModel = eventModel;
+
+            return View(events);
+        }
+
+        public ActionResult AddNewEvent()
+        {
+            EventModel model = new EventModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddNewEvent(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ID = eventModel.Count() + 1;
+                eventModel.Add(model);
+
+                return RedirectToAction("EventView", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult UpdateEvent(int ID)
+        {
+            EventModel event_model = eventModel.Find(x => x.ID == ID);
+
+            return View(event_model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEvent(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                EventModel event_model = eventModel.Find(x => x.ID == model.ID);
+                event_model.name = model.name;
+                event_model.location = model.location;
+
+                return RedirectToAction("EventView", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult DeleteEvent(int ID)
+        {
+            EventModel model = eventModel.Find(x => x.ID == ID);
+            eventModel.Remove(model);
+
+            return RedirectToAction("EventView", "Event");
+        }
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Models/EventModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab04.Models
+{
+    public class EventModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Името на настанот е задолжително.")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Локацијата на настанот е задолжителна.")]
+        [StringLength(30, MinimumLength = 5)]
+        public string location { get; set; }
+    }
+}
